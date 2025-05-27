bafaa8d2546d85a8df3f4d24ed84c9dfb54af22f
DESKTOP-VKBFIRM\ADPC <khoidragonslayer@gmail.com>
2025-05-27 10:32:34 +0700

    setup jest with msw

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,18 @@
+import { server } from "@/mocks/server";
 import "@testing-library/jest-dom";
+
+beforeAll(() => {
+  // Start the interception.
+  server.listen();
+});
+
+afterEach(() => {
+  // Remove any handlers you may have added
+  // in individual tests (runtime handlers).
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  // Disable request interception and clean up.
+  server.close();
+});
